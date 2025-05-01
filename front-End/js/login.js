f770e49491ce1ace6029de03db103c7c76db3697
helouazizi <ouazizi2code@gmail.com>
2025-05-01 19:08:15 +0100

    addd styling

--- front-End/js/login.js
+++ front-End/js/login.js
@@ -1,13 +1,25 @@
+import { register } from "./register.js";
+
 function login() {
   const login_btn = document.getElementById("login_btn");
-  login_btn.addEventListener("click", (e) => {
+  const login_btn_1 = document.getElementById("login_btn_1");
+  let result_btn = login_btn;
+  if (login_btn_1) {
+    result_btn = login_btn_1;
+  }
+  result_btn.addEventListener("click", (e) => {
     const container = document.getElementById("container");
     container.classList.add("modal-active");
 
+    // let remove the previoiise form
+    let prevform = document.getElementById("register_form");
+    if (prevform) {
+      prevform.remove();
+    }
     // let careate our form
     let form = document.createElement("div");
     form.setAttribute("class", "modal-overlay");
-    form.setAttribute("id", "login-modal");
+    form.setAttribute("id", "login_form");
     form.innerHTML = `
             <div class="modal-content">
             <button class="close-btn" id="close-form">&times;</button>
@@ -22,16 +34,20 @@ function login() {
             <input type="password" id="login_password" name="password" required />
 
             <button type="submit">Login</button>
+            </form>
             <div class="register_action"> 
                 <p>Don't have an account ? </p>
-                <button class="primary-btn " value="register"><i class="fas fa-user-plus"></i> register</button >
+                <button class="primary-btn" id="register_btn"><i class="fas fa-user-plus"></i> register</button >
             </div>
-            </form>
             </div>
         `;
 
     form.classList.add("active");
     document.body.appendChild(form);
+
+    /////////////////////////////
+    register();
+    /////////////////////////////
 
     /////////////////// handle the form caancling
     const close_btn = document.getElementById("close-form");
@@ -39,7 +55,6 @@ function login() {
       form.remove();
       container.classList.remove("modal-active");
     });
-    // }
   });
 }
 
--- front-End/js/main.js
+++ front-End/js/main.js
@@ -1,6 +1,8 @@
 
 
 import { login } from "./login.js"
-
+import { register } from "./register.js"
+register()
 login()
+// register()
 console.log("he from js")
